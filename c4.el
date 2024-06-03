5c37f8787432d0923fd64c54a09c6b2ca413affb
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2024-06-03 19:54:50 -0400

    Add Package-Requires

--- c4.el
+++ c4.el
@@ -4,6 +4,7 @@
 
 ;; Author: Nicholas Vollmer <nv@parenthetic.dev>
 ;; Keywords: games
+;; Package-Requires: ((emacs "29") (imp "0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
